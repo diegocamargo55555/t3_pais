5ca1fcf6a7165313f56e7d467b7df183e6b0957e
diegocamargo55555 <137927984+diegocamargo55555@users.noreply.github.com>
2025-11-26 15:25:05 -0300

    ajuda da bea

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:t3_pais/view/home_page.dart';
+import 'package:t3_pais/firebase_options.dart';
+import 'package:t3_pais/view/auth_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
-  // runApp(const MyApp());
+  runApp(MaterialApp(home: AuthPage(), debugShowCheckedModeBanner: false));
 }
